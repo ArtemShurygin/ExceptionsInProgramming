e2d192898424eb4cc8c08b3b1906f8d5be630c85
artem <shurtem@mail.ru>
2024-02-12 06:21:45 +0300

    Вернули ввод с консоли, добавили исключения, исправили проверку даты.

--- src/main.java
+++ src/main.java
@@ -25,6 +25,7 @@
 
 
 import java.io.BufferedWriter;
+import java.util.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.StringWriter;
@@ -48,33 +49,46 @@ public class main {
         int fioOk = -3;
 
         int i = 0;
+        String data = "-1";
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        // запрещает смещать дату, если введено большее количество дней/месяцев, чем возможно
+        dateFormat.setLenient(false);
+        //Date dateMax = new Date(2024, 01,01);
+        //Date dateMin = new Date(1900, 01, 01);
 
+        String dateMax = "01.01.2024";
+        String dateMin = "01.01.1900";
         ArrayList<String> FIO = new ArrayList<String>();
 
+        System.out.print("Введите следующие данные в произвольном порядке, разделенные пробелом:\n" + 
+        "Фамилия Имя Отчество датарождения номертелефона пол\n" + "\n" + 
+        "Форматы данных:\n" + 
+        "фамилия, имя, отчество - строки\n" + 
+        "датарождения - строка формата dd.mm.yyyy\n" + 
+        "номертелефона - целое беззнаковое число без форматирования\n" + 
+        "пол - символ латиницей f или m\n" + "\n" + 
+        "Введите данные: \n");
+        // String data = "Ivanov Ivan Ivanovich 23.10.1991 f +79112293344";
+        // String data = "Иванов Иван Иванович 23.10.1991 f +79112293344";  
 
-        // System.out.print("Введите данные: ");
-        // Иванов Иван Иванович 01.01.1991 87777777777
-        // Ivanov Ivan Ivanovich 01.01.1991 87777777777
-        // String str = sc.nextLine();
-        
-        String data = "Иванов Иван Иванович 23.10.1991 f +79112293344";
-        System.out.println(data);
-
+        try {
+            data = sc.nextLine();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        //System.out.println(data);
 
         // разделили строку на массив строк, где разднлителем является пробел
         String [] splitData = data.split(" ");
 
-
-
-
-
         for (String splitDataOne : splitData){
-            System.out.println(splitDataOne);
+            //System.out.println(splitDataOne);
 
             // проверка на наличе только букв
             if (Pattern.matches("[ЁёА-яa-zA-Z]+",splitDataOne)) { 
-                System.out.println("Только буквы");
+                //System.out.println("Только буквы");
 
                 // определили гендер
                 if (splitDataOne.equalsIgnoreCase("m") | splitDataOne.equalsIgnoreCase("f")){
@@ -88,7 +102,7 @@ public class main {
                 }
             }
             else{
-                System.out.println("Не только буквы");
+                //System.out.println("Не только буквы");
  
                 // проверка на номер телефона
                 if (phoneOk == -1){
@@ -104,19 +118,25 @@ public class main {
                             phoneOk = 0;
                         }
                         else{
-                            System.out.println("Неверный формат номера");
+                            throw new NumberFormatException("Неверный формат телефона, телефон должен начинаться с +7 или 8 и иметь 11 цифр (89112223344)");
                         }
                     }
                 }
 
                 // проверка на дату
                 if (dateOk == -1){
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
                     try {
-                        java.util.Date date = dateFormat.parse(splitDataOne);
-                        System.out.println("date = " + date);
-                        dateStr = splitDataOne;
-                        dateOk = 0;
+                        if(splitDataOne.length() == 10){
+                            java.util.Date date = dateFormat.parse(splitDataOne);
+                            //System.out.println("date = " + date);
+                            if(date.after(dateFormat.parse(dateMin)) && date.before(dateFormat.parse(dateMax))) {
+                                dateStr = splitDataOne;
+                                dateOk = 0;
+                            }
+                            else{
+                                throw new NumberFormatException("Дата рождения не попала во временные рамки 01.01.1900-01.01.2024");
+                            }
+                        }
                     }
                     catch (Exception e) {
                         System.out.println(e.getMessage());
@@ -125,6 +145,7 @@ public class main {
             }
         }
 
+        /* 
         // Вывод кодов ошибки
         System.out.println();
         System.out.println("fioOk = " + fioOk);
@@ -138,13 +159,27 @@ public class main {
         System.out.println("date = " + dateStr);
         System.out.println("phoneNumber = " + phoneNumber);
         System.out.println("Gender = " + gender);
-
         System.out.println();
+        */
+
+    if(fioOk != 0){
+        throw new RuntimeException("Не введены или неверно введены ФИО! ФИО включает 3 слова, разделенных пробелом, без цифр и символов");
+    }
+    if(dateOk != 0){
+        throw new RuntimeException("Не введена или неверно введена дата рождения! Формат дыты: dd.mm.yyyy");
+    }
+    if(phoneOk != 0){
+        throw new RuntimeException("Не введен или неверно введен номер телефона! Номер должен начинаться с +7 или 8 и иметь 11 цифр (89112223344)");
+    }
+    if(genderOk != 0){
+        throw new RuntimeException("Не введен или неверно введены пол! Пол - символ латиницей f или m");
+    }
+
         if(fioOk + dateOk + phoneOk + genderOk == 0){
-            System.out.println("allOk");
+            //System.out.println("allOk");
             String info = FIO.get(0) + " " + FIO.get(1) + " " +FIO.get(2) +
             " " + dateStr + " " + phoneNumber + " " + gender;
-            System.out.println(info);
+            //System.out.println(info);
 
             //проверка наличия файла относительно открытого проекта
             String thisDirectory = System.getProperty("user.dir");
@@ -152,23 +187,23 @@ public class main {
 
             try{
 
-            File file = new File(thisDirectory + "\\Data\\" + FIO.get(0) + ".txt");
+                File file = new File(thisDirectory + "\\Data\\" + FIO.get(0) + ".txt");
 
-            // Создаёт файл, если он отсутствует
-            if (!file.exists()) {
-                file.createNewFile();
+                // Создаёт файл, если он отсутствует
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+
+                FileWriter fw = new FileWriter(file,true); 
+                BufferedWriter bw = new BufferedWriter(fw);
+                fw.write(info);
+                bw.newLine();
+                bw.close();
+                System.out.println("Success");
             }
-
-            FileWriter fw = new FileWriter(file,true); 
-            BufferedWriter bw = new BufferedWriter(fw);
-            fw.write(info);
-            bw.newLine();
-            bw.close();
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-    
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
